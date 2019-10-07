0eb77a0bf8f0184dbb77552817950ceadab1210e
pablo <raselberg@gmail.com>
2019-10-06 19:30:38 -0700

    preCommit

---
+++ data/friendsdata.js
@@ -0,0 +1,23 @@
+var friendsArray = [
+  {
+    Name: "Anthony",
+    Photo: "http://www.classmates.com/blog/wp-content/uploads/2018/09/Tony-Shalhoub-1971-junior-portrait.png",
+    Score: ["5","4","3","2","1"],
+   
+  },
+  {
+    Name: "Trae",
+    Photo: "https://townsquare.media/site/812/files/2017/07/trae-tha-truth-paras-griffin.jpg",
+    Score: ["1","2","3","4","5"],
+   
+  },
+  {
+    Name: "Tray",
+    Photo: "https://www.ikea.com/PIAimages/0641688_PE700598_S5.JPG?f=s",
+    Score: ["2","3","4","5","1"],
+   
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
